6de22141bc22e5c271c063809a6136db59ec968d
qujinping <jjqquu@gmail.com>
2017-04-06 16:43:21 +0800

    Add openshift build config for the project

---
+++ build/catalogue.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+oc process -f /Users/qujinping/minishift/cicd/app/gobc_templ.yaml -v=APP_NAME=catalogue -vAPP_SOURCE_REPOSITORY_URL=https://github.com/qujinping/catalogue -vAPP_BUILDER_IMAGE=builder-go-16-centos7:latest | oc create -f -
---
+++ build/orders.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+oc process -f /Users/qujinping/minishift/cicd/app/javabc_templ.yaml -v=APP_NAME=orders -vAPP_SOURCE_REPOSITORY_URL=https://github.com/qujinping/orders -vAPP_BUILDER_IMAGE=builder-openjdk-18-centos7:latest | oc create -f -
